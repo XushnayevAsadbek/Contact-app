6bbb75e94c4aec0be99991c8c7b17bf6b30b763e
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-05-13 12:14:44 +0500

    contact css commit

--- src/components/layout/sidebar/sidebar.jsx
+++ src/components/layout/sidebar/sidebar.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom"
-
+import search from './img/search-outline.svg'
+import   './sidebar.css';
 export const Sidebar = ({ users }) => {
     return (
-        <div className="p-3 border-end vh-100 ">
-            <div className="mb-3 d-flex">
-                <input className=" form-control " type="text" />
-                <Link to="/add" className="btn  btn-outline-success ms-2 ">New</Link>
+        <div className="p-3 border-end vh-100 sidebar ">
+            <div className="mb-3 d-flex border-bottom pb-3">
+           <div className="d-flex position-relative">
+           <img className=" sidebar-icon " src={search}  width={15} height={15}/>
+                <input className="search-input " type="search" />
+           </div>
+                <Link to="/add" className=" btn btn-outline-secondary ms-2  ">New</Link>
             </div>
 
-          { (<ul>
+          { (<ul className="sidebar-item">
                 {
                     users.map((user)=>(
-                         <li  key={user.id}> 
-                    <Link to={`contact/${user.id}`}>{user.fullName} </Link>
+                         <li className="sidebar-item--list"  key={user.id}> 
+                    <Link className="sidebar-item--link" to={`contact/${user.id}`}>{user.fullName} </Link>
                     </li>
                     ))}
             </ul>
--- src/components/page/contact/contact.jsx
+++ src/components/page/contact/contact.jsx
@@ -1,40 +1,51 @@
-import { useParams,useNavigate } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import PropTypes from 'prop-types'
+import './contact.css'
 
-
- export const Contact =({users}) =>{
+export const Contact = ({ users }) => {
     const params = useParams();
-    console.log( params);
+    console.log(params);
     const navigate = useNavigate();
     const foundUser = users.find((user) => user.id === params.id)
 
-    if(!foundUser){
+    if (!foundUser) {
         return <>Error user not found </>
     }
 
-    const onEdit = () =>{
+    const onEdit = () => {
         navigate('/edit')
     }
-    
-    return(
-        <>
-       <img src={foundUser.img}  
-       width={350}
-       height={350}
-       style={{
-        objectFit: 'contain',
-       }}
-       />
 
-       <button onClick={onEdit}>Edit</button>
-       <button>Delete</button>
+    return (
+        <div className="contact">
+            <div>
+                <img className="contact-img" src={foundUser.img}
+                    width={200}
+                    height={200}
+                    style={{
+                        objectFit: 'contain',
+                        marginRight: '15px',
+                        borderRadius: '15px'
+                    }}
+                />
+            </div>
+            <div className="contact-founduser">
+                <div >
+                <p className="m-0 contact-name"> <span>Fullname : </span> {foundUser.fullName}</p>
+                <p className="contact-phone"><span>Phone : </span> {foundUser.phone}</p>
+                </div>
 
-        </>
+
+                <button className="contact__btn--edit" onClick={onEdit}>Edit</button>
+                <button className="contact__btn--deelet">Delete</button>
+            </div>
+
+        </div>
     );
 
-    
+
 };
 
-Contact .propTypes ={
-    users:PropTypes.array,
+Contact.propTypes = {
+    users: PropTypes.array,
 };
